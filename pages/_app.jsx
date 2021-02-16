75dcf400a84e163ab7428eb1bd1ab449a21b4dfd
Narudom <iam@bapairaew.com>
2021-02-16 16:31:29 +0800

    Add GA

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,25 @@
 import { ThemeProvider } from "theme-ui";
 import components from "~/theme/components";
 import theme from "~/theme/config";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import ga from "react-ga";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    ga.initialize("UA-131730401-1");
+
+    const logPageView = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", logPageView);
+    return () => {
+      router.events.off("routeChangeComplete", logPageView);
+    };
+  }, []);
   return (
     <ThemeProvider components={components} theme={theme}>
       <Component {...pageProps} />
