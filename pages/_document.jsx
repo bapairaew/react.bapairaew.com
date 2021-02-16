e1397fa5e57b9dfd81d80bb11b1fb471fec14b27
Narudom <iam@bapairaew.com>
2021-02-16 15:46:02 +0800

    Fix font not shown correctly

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,7 +13,7 @@ export default class extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
         </Head>
