1f81a154ac1aec8141ecaa2eac49926c31cf27ba
Narudom <iam@bapairaew.com>
2021-02-20 18:56:59 +0800

    Optimise fonts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,6 +1,9 @@
 import Document, { Main, NextScript, Head, Html } from "next/document";
 import { InitializeColorMode } from "theme-ui";
 
+const googleFontsUrl =
+  "https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap";
+
 export default class extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -11,10 +14,14 @@ export default class extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* https://csswizardry.com/2020/05/the-fastest-google-fonts/ */}
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preload" as="style" href={googleFontsUrl} />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
             rel="stylesheet"
+            href={googleFontsUrl}
+            media="print"
+            onload="this.media='all'"
           />
         </Head>
         <body>
