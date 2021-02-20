20dc4718acbc741eb78f87c29fb1483d2dd665a9
Narudom <iam@bapairaew.com>
2021-02-20 15:39:47 +0800

    Refactor analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,25 +1,11 @@
 import { ThemeProvider } from "theme-ui";
+import useAnalytics from "~/libs/analytics";
 import components from "~/theme/components";
 import theme from "~/theme/config";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import ga from "react-ga";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
+  useAnalytics();
 
-  useEffect(() => {
-    ga.initialize("UA-131730401-1");
-
-    const logPageView = (url) => {
-      ga.pageview(url);
-    };
-
-    router.events.on("routeChangeComplete", logPageView);
-    return () => {
-      router.events.off("routeChangeComplete", logPageView);
-    };
-  }, []);
   return (
     <ThemeProvider components={components} theme={theme}>
       <Component {...pageProps} />
