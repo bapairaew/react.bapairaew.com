d47401660eb9827238a023aa5718652dd05d654f
Narudom <iam@bapairaew.com>
2021-02-21 15:33:00 +0800

    Simplify

--- components/common/GlitchHeading.jsx
+++ components/common/GlitchHeading.jsx
@@ -1,73 +1,41 @@
 import { Box, Heading, useThemeUI } from "theme-ui";
-import { motion } from "framer-motion";
 
 export default function GlitchHeading({ ...props }) {
   const { theme } = useThemeUI();
   return (
     <Box sx={{ position: "relative" }}>
-      <motion.div
-        animate={[
-          {
-            clipPath: "inset(40% 0 61% 0)",
-          },
-          {
-            clipPath: "inset(92% 0 1% 0)",
-          },
-          {
-            clipPath: "inset(43% 0 1% 0)",
-          },
-          {
-            clipPath: "inset(25% 0 58% 0)",
-          },
-          {
-            clipPath: "inset(54% 0 7% 0)",
-          },
-          {
-            clipPath: "inset(58% 0 43% 0)",
-          },
-        ]}
-        transition={{ repeat: Infinity, duration: 0.5 }}
-        style={{
+      <Box
+        sx={{
           position: "absolute",
-          left: -2,
-          top: -2,
-          zIndex: -1,
+          left: -1,
+          top: 0,
+          clipPath: "inset(20% 0 70% 0)",
+          backgroundColor: "background",
+          animation: "blink-animation 500ms steps(5, start) infinite",
         }}
       >
         <Heading aria-hidden="true" {...props} />
-      </motion.div>
-      <motion.div
-        animate={[
-          {
-            clipPath: "inset(40% 0 61% 0)",
-          },
-          {
-            clipPath: "inset(92% 0 1% 0)",
-          },
-          {
-            clipPath: "inset(43% 0 1% 0)",
-          },
-          {
-            clipPath: "inset(25% 0 58% 0)",
-          },
-          {
-            clipPath: "inset(54% 0 7% 0)",
-          },
-          {
-            clipPath: "inset(58% 0 43% 0)",
-          },
-        ]}
-        transition={{ repeat: Infinity, duration: 0.5, delay: 0.2 }}
-        style={{
-          position: "absolute",
-          right: -2,
-          right: -2,
-          zIndex: -1,
-        }}
-      >
-        <Heading aria-hidden="true" {...props} />
-      </motion.div>
+      </Box>
       <Heading {...props} />
+      <Box
+        sx={{
+          position: "absolute",
+          right: -1,
+          top: 0,
+          clipPath: "inset(70% 0 20% 0)",
+          backgroundColor: "background",
+          animation: "blink-animation 500ms steps(5, start) infinite",
+        }}
+      >
+        <Heading aria-hidden="true" {...props} />
+      </Box>
+      <style jsx global>{`
+        @keyframes blink-animation {
+          to {
+            visibility: hidden;
+          }
+        }
+      `}</style>
     </Box>
   );
 }
