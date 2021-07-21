ef072ff66e0bc4647cd240a6bc93f11358b960bf
Narudom <iam@bapairaew.com>
2021-07-21 08:47:35 +0700

    Replace GA with plausible

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from "theme-ui";
-import useAnalytics from "~/libs/analytics";
 import components from "~/theme-ui/mdxComponents";
 import theme from "~/theme-ui/theme";
+import PlausibleProvider from "next-plausible";
+
+const { NEXT_PUBLIC_PLAUSIBLE_DOMAIN: domain } = process.env;
 
 export default function App({ Component, pageProps }) {
-  useAnalytics();
-
   return (
-    <ThemeProvider components={components} theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <PlausibleProvider enabled={!!domain} domain={domain}>
+      <ThemeProvider components={components} theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </PlausibleProvider>
   );
 }
