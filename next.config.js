95fa1f2190d5254ed58d66d0902a682c7219061f
Narudom <iam@bapairaew.com>
2021-07-21 09:05:02 +0700

    Use plausible proxy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-module.exports = require("next-mdx-enhanced")({
-  layoutPath: "components",
-})({
-  images: {
-    domains: [
-      "i.scdn.co", // Spotify Album Art
-      "m.media-amazon.com", // IMDB movie poster
-      "static.whiskybase.com", // Whiskybase bottle photo
-      "i.gr-assets.com", // Goodreads cover image
-    ],
-  },
-});
+const { withPlausibleProxy } = require("next-plausible");
+const mdxEnhanced = require("next-mdx-enhanced");
+
+module.exports = withPlausibleProxy()(
+  mdxEnhanced({
+    layoutPath: "components",
+  })({
+    images: {
+      domains: [
+        "i.scdn.co", // Spotify Album Art
+        "m.media-amazon.com", // IMDB movie poster
+        "static.whiskybase.com", // Whiskybase bottle photo
+        "i.gr-assets.com", // Goodreads cover image
+      ],
+    },
+  })
+);
